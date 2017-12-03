585435675efda8d5d50f32986b2955a2727e5541
minhalcollege <minhalcollege@gmail.com>
2017-12-03 19:34:31 +0200

    read titles

--- app/src/main/java/tomerbu/edu/rssdemo/RssHTTPHandler.java
+++ app/src/main/java/tomerbu/edu/rssdemo/RssHTTPHandler.java
@@ -1,5 +1,12 @@
 package tomerbu.edu.rssdemo;
 
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,6 +27,18 @@ public class RssHTTPHandler {
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
 
         InputStream in = con.getInputStream();
+        String xml = read(in, "utf8");
+        Document document = Jsoup.parse(xml);
+
+        Elements itemsArray = document.getElementsByTag("item");
+
+        for (Element e : itemsArray) {
+            //there is only one title element per item.
+            Element  title = e.getElementsByTag("title").first();
+            String titleValue = title.val();
+            System.out.println(titleValue);
+            Log.d("Rss", titleValue);
+        }
     }
     private static String read(InputStream in, String charset) throws IOException {
         StringBuilder builder = new StringBuilder();
